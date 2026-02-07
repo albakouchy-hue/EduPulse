4d707e3940db5c122365333cde5f60ee1bebf170
albakouchy-hue <albakouchy@gmail.com>
2026-02-07 13:00:14 +0100

    Create main.js

---
+++ frontend/frontend/assets/js/main.js
@@ -0,0 +1,11 @@
+function gotoTool(tool) {
+  alert("سيتم فتح الأداة: " + tool);
+}
+
+function exportPDF() {
+  alert("تصدير PDF جاري...");
+}
+
+function openAssistant() {
+  alert("المساعد الذكي جاهز!");
+}
